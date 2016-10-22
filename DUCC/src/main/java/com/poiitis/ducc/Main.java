53052e74186e77fe68cf14c1128815e1e80e46f9
mpoiitis <marinospoet@gmail.com>
2016-10-23 02:57:40 +0300

    Update main class

--- DUCC/src/main/java/com/poiitis/ducc/Main.java
+++ DUCC/src/main/java/com/poiitis/ducc/Main.java
@@ -1,8 +1,11 @@
 package com.poiitis.ducc;
 
+import com.poiitis.pli.PLIBuilder;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
+
 
 /**
  *
@@ -10,18 +13,21 @@ import org.apache.spark.api.java.JavaSparkContext;
  */
 public class Main {
     
-    private static final String INPUT_PATH = "hdfs://localhost:9000/user/marinos/input";
-    
+    private static final String INPUT_PATH = "hdfs://localhost:9000/user/marinos/input"; 
     
     public static void main(String[] args){
         
         SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> input = sc.textFile(INPUT_PATH);
-        
+ 
         Parser parser = new Parser(input);
         
-        JavaRDD<Adult> adults = parser.parseFile();//TODO persist rdd without error on stdout
+        LongAccumulator lineAcc = sc.sc().longAccumulator(); 
+        JavaRDD<Adult> adults = parser.parseFile(lineAcc);//TODO persist rdd without error on stdout
+        
+        PLIBuilder pliBuilder = new PLIBuilder(adults, true);
+        pliBuilder.createInitialPLIs();
     }
     
 }
